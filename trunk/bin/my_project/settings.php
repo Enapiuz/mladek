047b43574c07c67f02588fdba15c22abef2b921f
vstoupa <vstoupa@6759b15a-a74b-0410-9fea-279ec709e8d4>
2008-04-26 13:56:16 +0000

    typo: route articles/ changed to article/

---
+++ trunk/bin/my_project/settings.php
@@ -0,0 +1,18 @@
+<?php
+use mladek::core::conf;
+
+define('MLADEK_DIR', ''); # without slash on end
+
+require_once MLADEK_DIR.'/core/autoload.php';
+
+conf::set('PROJECT_DIR', __DIR__); # without slash on end
+
+conf::set('REWRITE_BASE', ''); # without slash on end
+
+//conf::add_include_path('d:/anywhere');
+
+
+// here will be settings
+
+
+@include_once 'settings_local.php';
