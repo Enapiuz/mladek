4223c7494913799a2bb64515b84312c06fd83ce3
vstoupa <vstoupa@6759b15a-a74b-0410-9fea-279ec709e8d4>
2008-04-26 13:53:53 +0000

    mladek::core:urls basic routing and reverse routing

--- trunk/core/urls.php
+++ trunk/core/urls.php
@@ -1,15 +1,49 @@
 <?php
 namespace mladek::core;
+use mladek::core::conf;
 
 final class urls
 {
+    static $urls = array();
+    
     static function patterns()
     {
-        foreach (func_get_args() as $pattern) {
-            if (!isset($pattern[2]) or !is_array($pattern[2])) {
-                $pattern[2] = array();
+    
+        $path = str_replace(
+                    conf::get('REWRITE_BASE').'/',
+                    '/',
+                    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+                    );
+        self::$urls = func_get_args();
+        foreach (self::$urls as $pattern) {
+            $pattern[0] = str_replace('^', '^/', $pattern[0]);
+            $pattern[0] = str_replace('/', '\\/', $pattern[0]);
+            if (preg_match('/'.$pattern[0].'/', $path)) {
+                if (!isset($pattern[2]) or !is_array($pattern[2])) {
+                    $pattern[2] = array();
+                }
+                call_user_func_array($pattern[1], $pattern[2]);
+                break;
             }
-            call_user_func_array($pattern[1], $pattern[2]);
+            throw new Exception('No url match');
         }
+    }
+    
+    static function reverse()
+    {
+        $params = func_get_args();
+        $name = array_shift($params);
+        foreach (self::$urls as $url) {
+            if ($name === $url['name'] or $name === $url[1]) {
+                $link = $url[0];
+                foreach ($params as $param) {
+                    $link = preg_replace('/\(.*\)/', $param, $link, 1);
+                }
+                $link = str_replace('^', '/', substr($link, 0, -1));
+                return $link;
+                break;
+            }
+        }
+
     }
 }
