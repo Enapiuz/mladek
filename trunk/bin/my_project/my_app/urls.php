65be851feae650a22dfca6f9160d254418a4c833
vstoupa <vstoupa@6759b15a-a74b-0410-9fea-279ec709e8d4>
2008-04-26 22:16:38 +0000

    complete with previous commit

--- trunk/bin/my_project/my_app/urls.php
+++
@@ -1,8 +0,0 @@
-<?php
-use mladek::conf::urls::urls;
-
-urls::patterns(array(
-
-    array('^(\d+)/$', 'views::article', 'name'=>'article'),
-    
-));
--- trunk/bin/my_project/urls.php
+++
@@ -1,9 +0,0 @@
-<?php
-use mladek::conf::urls::urls;
-
-urls::patterns(array(
-
-    array('^article/', urls::inc('my_project::my_app::urls')),
-    array('^$', 'my_project::my_app::views::my_method'),
-    
-));
