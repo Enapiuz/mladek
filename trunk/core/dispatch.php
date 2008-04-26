5fa9cfc558664d482bb0ccd6cbbf7d21500bf7a9
vstoupa <vstoupa@6759b15a-a74b-0410-9fea-279ec709e8d4>
2008-04-26 13:54:40 +0000

    - mladek::core::urls accept include urls - http::response404 added

--- trunk/core/dispatch.php
+++ trunk/core/dispatch.php
@@ -1,11 +1,18 @@
 <?php
 namespace mladek::core;
+use mladek::core::http;
 
 class dispatch
 {
     static function run()
     {
+        ob_start();
         include_once conf::get('PROJECT_DIR').'/urls.php';
+        try {
+            mladek::core::urls::route();
+        } catch (Exception $e) {
+            http::response404($e->getMessage());
+        }
     }
 }
 
